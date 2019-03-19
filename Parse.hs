620644ca9f8ca8d5c43914efd3e58693fdf422b5
yoshiyaohno <yoshiyaohno@gmail.com>
2019-03-19 14:18:29 -0400

    rework wArgs functions and set up the dispatches

--- Parse.hs
+++ Parse.hs
@@ -9,43 +9,71 @@ import qualified Data.Map.Strict as M
 
 type DrawMats = (Screen, T.Transform Double, [Vect Double])
 
-noArgs  = ["ident", "apply", "display", "save"]
+noArgs :: (MonadState DrawMats m, MonadIO m) => [(String, m ())]
+noArgs = [ ("ident", ident)
+         , ("apply", apply)
+         , ("display", display)
+         , ("clear", clear)
+         ]
+
+wArgs :: (MonadState DrawMats m, MonadIO m) => [(String, [String] -> m ())]
+wArgs = [ ("save", save)
+        , ("line", line)
+        , ("scale", scale)
+        , ("move", move)
+        ]
 
 main = do
     args <- getArgs
     script <- readFile (head args)
-    putStrLn "oof"
+    let cmds = parse $ lines script :: [StateT DrawMats IO ()]
+    runState (sequence_ cmds) (M.empty, T.ident, [])
 
 parse :: (MonadState DrawMats m, MonadIO m) => [String] -> [m ()]
-parse (a:b:xs)
-    | a `elem` noArgs = 
+parse []  = []
+parse [a] =
+    case lookup a noArgs of
+        Just c  -> [c]
+        Nothing -> []
+parse (a:b:xs) =
+    case lookup a noArgs of
+        Just c0 -> c0 : (parse (b:xs))
+        Nothing -> 
+            case lookup a wArgs of
+                Just c1 -> (c1 $ words b) : (parse xs)
+                Nothing -> parse xs
 
-save :: (MonadState DrawMats m, MonadIO m) => String -> m ()
-save path = do
+save :: (MonadState DrawMats m, MonadIO m) => [String] -> m ()
+save args = do
+    let path = head args
     (scrn, _, _) <- get
-    display red
+    display
     liftIO $ writeFile path (printPixels (500, 500) scrn)
 
-display :: (MonadState DrawMats m) => Color -> m ()
-display color = modify $
+display :: (MonadState DrawMats m) => m ()
+display = modify $
     \(scrn, tform, edges) ->
-        (T.drawEdges color edges M.empty, tform, edges)
+        (T.drawEdges red edges M.empty, tform, edges)
 
-line :: (MonadState DrawMats m) => Line Double -> m ()
-line ln = modify $
-    \(s, t, edges) -> (s, t, addLine ln edges)
+line :: (MonadState DrawMats m) => [String] -> m ()
+line args = do
+    let [x0, y0, z0, x1, y1, z1] = map read args
+        ln = Line (Vect x0 y0 z0 1) (Vect x1 y1 z1 1)
+            in modify $ \(s, t, edges) -> (s, t, addLine ln edges)
 
 ident :: (MonadState DrawMats m) => m ()
 ident = modify $
     \(s, _, es) -> (s, T.ident, es)
 
-scale :: (MonadState DrawMats m) => Double -> Double -> Double -> m ()
-scale x y z = modify $
+scale :: (MonadState DrawMats m) => [String] -> m ()
+scale args = modify $
     \(scrn, tform, edges) -> (scrn, T.scale x y z <> tform, edges)
+    where [x, y, z] = map read args
 
-move :: (MonadState DrawMats m) => Double -> Double -> Double -> m ()
-move x y z = modify $
+move :: (MonadState DrawMats m) => [String] -> m ()
+move args = modify $
     \(scrn, tform, edges) -> (scrn, T.trans x y z <> tform, edges)
+    where [x, y, z] = map read args
 
 clear :: (MonadState DrawMats m) => m ()
 clear = modify $
